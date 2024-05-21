709aae9dc825d53e63c08d745eb7af4f4627157d
amigos-dos-jardinetes <153563864+amigos-dos-jardinetes@users.noreply.github.com>
2024-05-20 23:29:58 -0300

    MapaAccept
    Início do desenvolvimento do Mapa na página de Accept!!

--- src/pages/Accept/index.js
+++ src/pages/Accept/index.js
@@ -1,11 +1,14 @@
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import React, { useEffect, useState} from 'react';
+import { View, Image, Text, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import { getFirestore, collection, addDoc, updateDoc, doc, arrayUnion } from 'firebase/firestore';
+import { getFirestore, collection, addDoc, updateDoc, doc, arrayUnion, getDocs } from 'firebase/firestore';
 import { styles } from '../Accept/styles';
-import Form from '../Form/index'; 
+import markerImage from '../../assets/marker.png';
+import { MapContainer, TileLayer, Marker, Popup, Circle, useMapEvents } from 'react-leaflet';
+import { userSearchData } from '../../../functions';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBe8nNAzDIXpriQ2fqE7QFHAMtETRbiN84",
@@ -16,13 +19,64 @@ const firebaseConfig = {
   appId: "1:381072997535:web:157abb3a076162a90836aa"
 };
 
-const firebaseApp = initializeApp(firebaseConfig);
-const auth = getAuth(firebaseApp);
-const firestore = getFirestore(firebaseApp);
+
+
+
+const customIcon = new L.Icon({
+  iconUrl: markerImage,
+  iconSize: [32, 32], // Defina o tamanho do ícone conforme necessário
+  iconAnchor: [16, 16], // Defina o ponto de ancoragem do ícone
+});
+
+function MapWithClick({ onClick }) {
+  useMapEvents({
+    click(e) {
+      onClick(e.latlng);
+    },
+  });
+  return null;
+}
+
+
 
 export default function Accept() {
+  const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
+const firestore = getFirestore(firebaseApp);
+const storage = getStorage();
   const navigation = useNavigation();
   const [novoJardineteDocId, setNovoJardineteDocId] = useState(null); // Estado para armazenar o ID do documento do jardinete
+  const [jardinetes, setJardinetes] = useState([]);
+  const [tempMarkerPosition, setTempMarkerPosition] = useState(null);
+  const [userName, setUserName] = useState('');
+const [wallpaper, setWallpaper] = useState(null);
+const [imageUrl, setImageUrl] = useState(null);
+const [email, setEmail] = useState('');
+const [pracasSeguidas, setPracasSeguidas] = useState([]);
+
+
+  useEffect(() => {
+    const fetchJardinetes = async () => {
+      const jardinetesCollection = collection(firestore, 'jardinetes');
+      const jardinetesSnapshot = await getDocs(jardinetesCollection);
+      const jardinetesData = jardinetesSnapshot.docs.map(doc => {
+        return { id: doc.id, ...doc.data() }; // Adicione o ID do documento ao objeto jardim
+      });
+      setJardinetes(jardinetesData);
+    };
+
+    fetchJardinetes();
+  }, []);
+
+  useEffect(() => {
+    const unsubscribe = userSearchData(auth, firestore, storage, navigation, setUserName, setWallpaper, setImageUrl, setEmail, setPracasSeguidas);
+  
+    return () => {
+      if (unsubscribe && typeof unsubscribe === 'function') {
+        unsubscribe();
+      }
+    };
+  }, []);
 
   const iniciarEnvioJardinete = async () => {
     try {
@@ -53,15 +107,82 @@ export default function Accept() {
 
   return (
     <View style={styles.container}>
-    <Text style={styles.title}>Você deseja iniciar o envio de um novo jardinete?</Text>
-    <View style={styles.buttonContainer}>
-      <TouchableOpacity style={styles.button} onPress={iniciarEnvioJardinete}>
-        <Text style={styles.buttonText}>Iniciar</Text>
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.button} onPress={voltarParaMenu}>
-        <Text style={styles.buttonText}>Voltar</Text>
-      </TouchableOpacity>
+
+<View style={styles.navbar}>
+            <View style={styles.imageContainer}>
+              {imageUrl ? (
+                <Image
+                  style={styles.logoImage}
+                  source={{
+                    uri: imageUrl,
+                  }}
+                />
+              ) : (
+                <Text>Imagem não disponível</Text>
+              )}
+            </View>
+          </View>
+    <View style={styles.mapContainer}>
+    <MapContainer
+        center={[-25.4284, -49.2733]}
+        zoom={14}
+        style={{ width: '100%', height: '100%', borderRadius: 10 }}
+      >
+        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+        {jardinetes.map((jardinete, index) => (
+          <React.Fragment key={index}>
+            <Marker position={[jardinete.coordenadas[0], jardinete.coordenadas[1]]} icon={customIcon}>
+              <Popup>
+                <div>
+                  <p>{jardinete.nome}</p>
+                  <img src={jardinete.jardinetePhoto} alt={`Foto de ${jardinete.nome}`} style={styles.popupImage} />
+                  <TouchableOpacity onPress={() => handleDetailsPress(jardinete)} style={styles.popupButton}>
+                    <Text style={styles.popupButtonText}>Selecionar Jardinete</Text>
+                  </TouchableOpacity>
+                </div>
+              </Popup>
+            </Marker>
+            <Circle
+              center={[jardinete.coordenadas[0], jardinete.coordenadas[1]]}
+              radius={28.28}
+              color="blue"
+              fillColor="blue"
+              fillOpacity={0.2}
+            />
+          </React.Fragment>
+        ))}
+
+        <MapWithClick onClick={setTempMarkerPosition} />
+
+        {tempMarkerPosition && (
+          <>
+            <Marker position={tempMarkerPosition} icon={customIcon}>
+              <Popup>Posição temporária</Popup>
+            </Marker>
+            <Circle
+              center={tempMarkerPosition}
+              radius={28.28}
+              color="red"
+              fillColor="red"
+              fillOpacity={0.2}
+            />
+          </>
+        )}
+      </MapContainer>
+
     </View>
-  </View>
+    <View style={styles.title1}>
+      <Text style={styles.title}>Você deseja iniciar o envio de um novo jardinete?</Text>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={iniciarEnvioJardinete}>
+          <Text style={styles.buttonText}>Iniciar</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button} onPress={voltarParaMenu}>
+          <Text style={styles.buttonText}>Voltar</Text>
+        </TouchableOpacity>
+      </View>
+      </View>
+    </View>
   );
 };
--- src/pages/Accept/styles.js
+++ src/pages/Accept/styles.js
@@ -3,18 +3,72 @@ import { StyleSheet, Dimensions } from 'react-native';
 const { width, height } = Dimensions.get('window');
 
 export const styles = StyleSheet.create({
- 
+
+
     container: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
         paddingHorizontal: 20,
       },
+
+      mapContainer: {
+
+        position: 'absolute',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: width * 0.9,
+        height: 600,
+        backgroundColor: 'red',
+        top: 400,
+        borderRadius: width * 0.0052083333333333,
+        borderColor: '#271C00',
+        borderWidth: width * 0.003125,
+      },
+
+      navbar: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        height: width * 0.06814583333333333333333333333333,
+        backgroundColor: '#166034',
+        position: 'absolute',
+        top: 0,
+        right: 0,
+        width: '100%',
+        zIndex: 2,
+      },
+    
+      navbarButton: {
+        fontSize: width * 0.0166666666666667,
+        color: '#FFF',
+        fontWeight: 'bold'
+      },
+
+      imageContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        marginRight: width * 0.0104166666666667, // Adiciona margem à direita
+      },
+
+      logoImage: {
+        width: width * 0.0520833333333333,
+        height: width * 0.0520833333333333,
+        borderRadius: width * 0.0260416666666667,
+      },
+
       title: {
         fontSize: 24,
         fontWeight: 'bold',
         marginBottom: 30,
         textAlign: 'center',
+      },
+
+      title1: {
+        top: 600,
+        alignItems: 'center',
       },
       buttonContainer: {
         flexDirection: 'row',
