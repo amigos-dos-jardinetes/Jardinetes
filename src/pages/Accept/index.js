687b08473dd98342e6313c32e23ff43e428e2bca
amigos-dos-jardinetes <153563864+amigos-dos-jardinetes@users.noreply.github.com>
2024-05-21 17:45:27 -0300

    Accept Concluído
    Página de Accept concluída!!!

--- src/pages/Accept/index.js
+++ src/pages/Accept/index.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState} from 'react';
-import { View, Image, Text, TouchableOpacity, StyleSheet, ScrollView } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Image, Text, TouchableOpacity, StyleSheet, Dimensions } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
@@ -9,6 +9,8 @@ import markerImage from '../../assets/marker.png';
 import { MapContainer, TileLayer, Marker, Popup, Circle, useMapEvents } from 'react-leaflet';
 import { userSearchData } from '../../../functions';
 import { getStorage } from 'firebase/storage';
+import { getDistance } from 'geolib';
+import { Ionicons } from '@expo/vector-icons'; // Certifique-se de ter o pacote @expo/vector-icons instalado
 
 const firebaseConfig = {
   apiKey: "AIzaSyBe8nNAzDIXpriQ2fqE7QFHAMtETRbiN84",
@@ -19,13 +21,10 @@ const firebaseConfig = {
   appId: "1:381072997535:web:157abb3a076162a90836aa"
 };
 
-
-
-
 const customIcon = new L.Icon({
   iconUrl: markerImage,
-  iconSize: [32, 32], // Defina o tamanho do ícone conforme necessário
-  iconAnchor: [16, 16], // Defina o ponto de ancoragem do ícone
+  iconSize: [32, 32],
+  iconAnchor: [16, 16],
 });
 
 function MapWithClick({ onClick }) {
@@ -37,30 +36,30 @@ function MapWithClick({ onClick }) {
   return null;
 }
 
-
-
 export default function Accept() {
   const firebaseApp = initializeApp(firebaseConfig);
-const auth = getAuth(firebaseApp);
-const firestore = getFirestore(firebaseApp);
-const storage = getStorage();
+  const auth = getAuth(firebaseApp);
+  const firestore = getFirestore(firebaseApp);
+  const storage = getStorage();
   const navigation = useNavigation();
-  const [novoJardineteDocId, setNovoJardineteDocId] = useState(null); // Estado para armazenar o ID do documento do jardinete
+  const [novoJardineteDocId, setNovoJardineteDocId] = useState(null);
   const [jardinetes, setJardinetes] = useState([]);
   const [tempMarkerPosition, setTempMarkerPosition] = useState(null);
   const [userName, setUserName] = useState('');
-const [wallpaper, setWallpaper] = useState(null);
-const [imageUrl, setImageUrl] = useState(null);
-const [email, setEmail] = useState('');
-const [pracasSeguidas, setPracasSeguidas] = useState([]);
-
+  const [wallpaper, setWallpaper] = useState(null);
+  const [imageUrl, setImageUrl] = useState(null);
+  const [email, setEmail] = useState('');
+  const [pracasSeguidas, setPracasSeguidas] = useState([]);
+  const [verificationMessage, setVerificationMessage] = useState('');
+  const [isVerificationSuccessful, setIsVerificationSuccessful] = useState(false);
+  const { width, height } = Dimensions.get('window');
 
   useEffect(() => {
     const fetchJardinetes = async () => {
       const jardinetesCollection = collection(firestore, 'jardinetes');
       const jardinetesSnapshot = await getDocs(jardinetesCollection);
       const jardinetesData = jardinetesSnapshot.docs.map(doc => {
-        return { id: doc.id, ...doc.data() }; // Adicione o ID do documento ao objeto jardim
+        return { id: doc.id, ...doc.data() };
       });
       setJardinetes(jardinetesData);
     };
@@ -70,7 +69,7 @@ const [pracasSeguidas, setPracasSeguidas] = useState([]);
 
   useEffect(() => {
     const unsubscribe = userSearchData(auth, firestore, storage, navigation, setUserName, setWallpaper, setImageUrl, setEmail, setPracasSeguidas);
-  
+
     return () => {
       if (unsubscribe && typeof unsubscribe === 'function') {
         unsubscribe();
@@ -83,7 +82,9 @@ const [pracasSeguidas, setPracasSeguidas] = useState([]);
       const user = auth.currentUser;
       if (user) {
         const jardinetesRef = collection(firestore, 'jardinetes');
-        const novoJardineteDocRef = await addDoc(jardinetesRef, {});
+        const novoJardineteDocRef = await addDoc(jardinetesRef, {
+          coordenadas: [tempMarkerPosition.lat, tempMarkerPosition.lng]
+        });
 
         const userDocRef = doc(firestore, 'users', user.uid);
         await updateDoc(userDocRef, {
@@ -91,13 +92,37 @@ const [pracasSeguidas, setPracasSeguidas] = useState([]);
         });
 
         console.log('Novo jardinete criado com sucesso!');
-        setNovoJardineteDocId(novoJardineteDocRef.id); // Defina o ID do novo documento do jardinete no estado
-        navigation.navigate('Form', { novoJardineteDocId: novoJardineteDocRef.id }); // Redireciona para a página de Form e passa o ID do novo documento como parâmetro
+        setNovoJardineteDocId(novoJardineteDocRef.id);
+        navigation.navigate('Form', { novoJardineteDocId: novoJardineteDocRef.id });
       } else {
         console.log('Nenhum usuário logado.');
       }
     } catch (error) {
       console.error('Erro ao iniciar o envio do jardinete:', error);
+    }
+  };
+
+  const verificarProximidade = () => {
+    if (!tempMarkerPosition) {
+      setVerificationMessage('Nenhum local selecionado. Por favor, escolha um local no mapa.');
+      setIsVerificationSuccessful(false);
+      return;
+    }
+
+    const isTooClose = jardinetes.some(jardinete => {
+      const distance = getDistance(
+        { latitude: tempMarkerPosition.lat, longitude: tempMarkerPosition.lng },
+        { latitude: jardinete.coordenadas[0], longitude: jardinete.coordenadas[1] }
+      );
+      return distance < 56.56;
+    });
+
+    if (isTooClose) {
+      setVerificationMessage('Já existe um jardinete próximo ao local selecionado.');
+      setIsVerificationSuccessful(false);
+    } else {
+      setVerificationMessage('');
+      setIsVerificationSuccessful(true);
     }
   };
 
@@ -107,82 +132,107 @@ const [pracasSeguidas, setPracasSeguidas] = useState([]);
 
   return (
     <View style={styles.container}>
-
-<View style={styles.navbar}>
-            <View style={styles.imageContainer}>
-              {imageUrl ? (
-                <Image
-                  style={styles.logoImage}
-                  source={{
-                    uri: imageUrl,
-                  }}
-                />
-              ) : (
-                <Text>Imagem não disponível</Text>
-              )}
-            </View>
-          </View>
-    <View style={styles.mapContainer}>
-    <MapContainer
-        center={[-25.4284, -49.2733]}
-        zoom={14}
-        style={{ width: '100%', height: '100%', borderRadius: 10 }}
-      >
-        <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-
-        {jardinetes.map((jardinete, index) => (
-          <React.Fragment key={index}>
-            <Marker position={[jardinete.coordenadas[0], jardinete.coordenadas[1]]} icon={customIcon}>
-              <Popup>
-                <div>
-                  <p>{jardinete.nome}</p>
-                  <img src={jardinete.jardinetePhoto} alt={`Foto de ${jardinete.nome}`} style={styles.popupImage} />
-                  <TouchableOpacity onPress={() => handleDetailsPress(jardinete)} style={styles.popupButton}>
-                    <Text style={styles.popupButtonText}>Selecionar Jardinete</Text>
-                  </TouchableOpacity>
-                </div>
-              </Popup>
-            </Marker>
-            <Circle
-              center={[jardinete.coordenadas[0], jardinete.coordenadas[1]]}
-              radius={28.28}
-              color="blue"
-              fillColor="blue"
-              fillOpacity={0.2}
+      <View style={styles.navbar}>
+        <TouchableOpacity style={styles.backButton} onPress={() => navigation.goBack()}>
+          <Ionicons name="arrow-back" size={width * 0.025} color="black" />
+        </TouchableOpacity>
+        <View style={styles.imageContainer}>
+          {imageUrl ? (
+            <Image
+              style={styles.logoImage}
+              source={{
+                uri: imageUrl,
+              }}
             />
-          </React.Fragment>
-        ))}
+          ) : (
+            <Text>Imagem não disponível</Text>
+          )}
+        </View>
+      </View>
 
-        <MapWithClick onClick={setTempMarkerPosition} />
+      <View style={styles.instructions}>
+          <Text style={styles.title2}>Por favor, selecione o local desejado para iniciar o envio do jardinete e verifique se é possível enviá-lo!</Text>
+      </View>
 
-        {tempMarkerPosition && (
-          <>
-            <Marker position={tempMarkerPosition} icon={customIcon}>
-              <Popup>Posição temporária</Popup>
-            </Marker>
-            <Circle
-              center={tempMarkerPosition}
-              radius={28.28}
-              color="red"
-              fillColor="red"
-              fillOpacity={0.2}
-            />
-          </>
+      <View style={styles.mapContainer}>
+        <MapContainer
+          center={[-25.4284, -49.2733]}
+          zoom={14}
+          style={{ width: '100%', height: '100%', borderRadius: 10 }}
+        >
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+          {jardinetes.map((jardinete, index) => (
+            <React.Fragment key={index}>
+              <Marker position={[jardinete.coordenadas[0], jardinete.coordenadas[1]]} icon={customIcon}>
+                <Popup>
+                  <div>
+                    <p>{jardinete.nome}</p>
+                    <img src={jardinete.jardinetePhoto} alt={`Foto de ${jardinete.nome}`} style={styles.popupImage} />
+                    <TouchableOpacity onPress={() => handleDetailsPress(jardinete)} style={styles.popupButton}>
+                  
+                    </TouchableOpacity>
+                  </div>
+                </Popup>
+              </Marker>
+              <Circle
+                center={[jardinete.coordenadas[0], jardinete.coordenadas[1]]}
+                radius={28.28}
+                color="blue"
+                fillColor="blue"
+                fillOpacity={0.2}
+              />
+            </React.Fragment>
+          ))}
+
+          <MapWithClick onClick={setTempMarkerPosition} />
+
+          {tempMarkerPosition && (
+            <>
+              <Marker position={tempMarkerPosition} icon={customIcon}>
+              
+              </Marker>
+              <Circle
+                center={tempMarkerPosition}
+                radius={28.28}
+                color="red"
+                fillColor="red"
+                fillOpacity={0.2}
+              />
+            </>
+          )}
+        </MapContainer>
+      </View>
+      <View style={styles.title1}>
+        <TouchableOpacity style={styles.button} onPress={verificarProximidade}>
+          <Text style={styles.buttonText}>Verificar</Text>
+        </TouchableOpacity>
+        {verificationMessage ? (
+          <Text style={styles.title}>{verificationMessage}</Text>
+        ) : (
+          isVerificationSuccessful && (
+            <>
+              <Text style={styles.title}>Continuar com o envio de um novo jardinete?</Text>
+              <View style={styles.buttonContainer}>
+                <TouchableOpacity style={styles.button} onPress={iniciarEnvioJardinete}>
+                  <Text style={styles.buttonText}>Continuar</Text>
+                </TouchableOpacity>
+                
+              </View>
+            </>
+          )
         )}
-      </MapContainer>
+      </View>
 
-    </View>
-    <View style={styles.title1}>
-      <Text style={styles.title}>Você deseja iniciar o envio de um novo jardinete?</Text>
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button} onPress={iniciarEnvioJardinete}>
-          <Text style={styles.buttonText}>Iniciar</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.button} onPress={voltarParaMenu}>
-          <Text style={styles.buttonText}>Voltar</Text>
-        </TouchableOpacity>
+      <View style={styles.imageContainer33}>
+          <Image source={require('../../assets/araucarias.png')}  style={styles.araucarias} />
+      </View>
+
+      <View style={styles.navbar2}>
+      <View style={styles.imageContainer22}>
+          <Image source={require('../../assets/UtfprBottom.png')}  style={styles.utfprImage} />
       </View>
       </View>
     </View>
   );
-};
+}
--- src/pages/Accept/styles.js
+++ src/pages/Accept/styles.js
@@ -9,7 +9,7 @@ export const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        paddingHorizontal: 20,
+        backgroundColor: '#FFFEF4',
       },
 
       mapContainer: {
@@ -18,13 +18,37 @@ export const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         width: width * 0.9,
-        height: 600,
-        backgroundColor: 'red',
-        top: 400,
+        height: width * 0.3125,
+        top: width * 0.2083333333333333,
         borderRadius: width * 0.0052083333333333,
         borderColor: '#271C00',
         borderWidth: width * 0.003125,
       },
+
+      instructions: {
+        position: 'absolute',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: width * 0.9,
+        top: width * 0.0859375,
+        height: width * 0.1041666666666667,
+        backgroundColor: '#B68F40' ,
+        borderRadius: width * 0.0104166666666667,      
+      },
+
+      
+  imageContainer33: {
+    top: width * 0.555,
+    position: 'absolute',
+    flexDirection: 'row',
+    alignItems: 'center',
+    left: width * 0.8333333333333333,
+  },
+
+  araucarias: {
+    width: width * 0.1651041666666667 * 0.8,
+    height: width * 0.146875 * 0.8,
+  },
 
       navbar: {
         flexDirection: 'row',
@@ -45,6 +69,39 @@ export const styles = StyleSheet.create({
         fontWeight: 'bold'
       },
 
+      imageContainer22: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'center', 
+    
+      },
+    
+
+      navbar2: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        height: width * 0.06814583333333333333333333333333,
+        backgroundColor: '#166034',
+        position: 'absolute',
+        bottom: 0,
+        top: width * 0.6510416666666667,
+        right: 0,
+        width: '100%',
+
+      },
+
+      utfprImage: {
+        width: 144 * (width * 0.00067708333),
+        height:57 * (width * 0.00067708333),
+        marginLeft: width * 0.0208333333333333,
+      },
+
+      backButton: {
+        marginLeft: width * 0.0052083333333333,
+      },
+
       imageContainer: {
         flex: 1,
         flexDirection: 'row',
@@ -60,16 +117,27 @@ export const styles = StyleSheet.create({
       },
 
       title: {
-        fontSize: 24,
+        fontSize: width * 0.0125,
         fontWeight: 'bold',
-        marginBottom: 30,
+        marginBottom: width * 0.0104166666666667,
+        marginTop: width * 0.0104166666666667,
         textAlign: 'center',
       },
 
       title1: {
-        top: 600,
+        top: width * 0.5416666666666667,
         alignItems: 'center',
+        position: 'absolute',
       },
+
+      title2: {
+        fontSize: width * 0.0166666666666667,
+        color: 'white',
+        marginBottom: width * 0.0104166666666667,
+        marginTop: width * 0.0104166666666667,
+        textAlign: 'center',
+      },
+      
       buttonContainer: {
         flexDirection: 'row',
      
@@ -77,15 +145,20 @@ export const styles = StyleSheet.create({
 
       button: {
         backgroundColor: '#166034',
-        paddingVertical: 10,
-        paddingHorizontal: 20,
-        borderRadius: 5,
-        marginEnd: 10,
+        paddingVertical: width * 0.0052083333333333,
+        paddingHorizontal: width * 0.0104166666666667,
+        borderRadius: width * 0.0026041666666667,
+        marginEnd: width * 0.0052083333333333,
       },
       buttonText: {
         color: 'white',
-        fontSize: 18,
+        fontSize: width * 0.009375,
         fontWeight: 'bold',
       },
+
+      popupImage: {
+        width: '200px',
+        height: '130px',
+    },
 
 });
