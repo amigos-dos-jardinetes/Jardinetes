c8a16a91af12ff3c537197712769f0895ad481ac
amigos-dos-jardinetes <amigos.dos.jardinetes@gmail.com>
2024-10-23 20:31:47 -0300

    Render Contato
    Solução da renderização encontrada e aplicada na página de contato.

--- src/pages/Contato/index.js
+++ src/pages/Contato/index.js
@@ -6,7 +6,7 @@ import { useNavigation } from '@react-navigation/native';
 export default function Contato() {
   const navigation = useNavigation();
   const scrollViewRef = useRef(null);
-
+  const myStyles = styles();
   const openExternalLink = () => {
     const externalLink = 'https://www.instagram.com/amigosdosjardinetes.ct/'; // Substitua com o link desejado
 
@@ -27,66 +27,66 @@ export default function Contato() {
       horizontal  // Enable horizontal scrolling
     >
       <ScrollView
-        contentContainerStyle={styles.scrollViewContent}
+        contentContainerStyle={myStyles.scrollViewContent}
         ref={scrollViewRef}
       >
         <ImageBackground
           source={require('../../assets/contato.jpg')}
-          style={styles.backgroundImage}
+          style={myStyles.backgroundImage}
         >
 
-          <View style={styles.navbar}>
+          <View style={myStyles.navbar}>
             <TouchableOpacity onPress={() => navigation.replace('PaginaInicial')}>
-              <Text style={styles.navbarButton}>PÁGINA INICIAL</Text>
+              <Text style={myStyles.navbarButton}>PÁGINA INICIAL</Text>
             </TouchableOpacity>
 
             <TouchableOpacity onPress={() => navigation.replace('acoesSociais')}>
-              <Text style={styles.navbarButton}>AÇÕES SOCIAIS</Text>
+              <Text style={myStyles.navbarButton}>AÇÕES SOCIAIS</Text>
             </TouchableOpacity>
 
             <TouchableOpacity onPress={() => navigation.replace('JardinetesMap')}>
-              <Text style={styles.navbarButton}>FAÇA SUA PARTE</Text>
+              <Text style={myStyles.navbarButton}>FAÇA SUA PARTE</Text>
             </TouchableOpacity>
 
             <TouchableOpacity onPress={() => navigation.replace('quemSomos')}>
-              <Text style={styles.navbarButton}>QUEM SOMOS</Text>
+              <Text style={myStyles.navbarButton}>QUEM SOMOS</Text>
             </TouchableOpacity>
 
             <TouchableOpacity onPress={() => navigation.replace('Contato')}>
-              <Text style={styles.navbarButton}>CONTATO</Text>
+              <Text style={myStyles.navbarButton}>CONTATO</Text>
             </TouchableOpacity>
 
             <TouchableOpacity onPress={() => navigation.replace('SignIn')}>
-              <Text style={styles.navbarButton}>LOGIN</Text>
+              <Text style={myStyles.navbarButton}>LOGIN</Text>
             </TouchableOpacity>
           </View>
 
 
-          <View style={styles.card}>
+          <View style={myStyles.card}>
 
             <TouchableOpacity onPress={openEmailComposer}>
-              <Image source={require('../../assets/Email1.png')} style={styles.buttonEmail} />
-              <Text style={styles.buttonText}>Email</Text>
+              <Image source={require('../../assets/Email1.png')} style={myStyles.buttonEmail} />
+              <Text style={myStyles.buttonText}>Email</Text>
             </TouchableOpacity>
 
             <TouchableOpacity onPress={openExternalLink}>
-              <Image source={require('../../assets/Instagram1.png')} style={styles.buttonInstagram} />
-              <Text style={styles.buttonText2}>Instagram</Text>
+              <Image source={require('../../assets/Instagram1.png')} style={myStyles.buttonInstagram} />
+              <Text style={myStyles.buttonText2}>Instagram</Text>
             </TouchableOpacity>
 
           </View>
-          <View style={styles.imageContainer33}>
-          <Image source={require('../../assets/araucarias.png')}  style={styles.araucarias} />
+          <View style={myStyles.imageContainer33}>
+          <Image source={require('../../assets/araucarias.png')}  style={myStyles.araucarias} />
         
       </View>
 
-          <View style={styles.imageContainer44}>
-          <Image source={require('../../assets/araucarias.png')}  style={styles.araucarias2} />
+          <View style={myStyles.imageContainer44}>
+          <Image source={require('../../assets/araucarias.png')}  style={myStyles.araucarias2} />
         
       </View>
-        <View style={styles.navbar2}>
-      <View style={styles.imageContainer22}>
-          <Image source={require('../../assets/UtfprBottom.png')}  style={styles.utfprImage} />
+        <View style={myStyles.navbar2}>
+      <View style={myStyles.imageContainer22}>
+          <Image source={require('../../assets/UtfprBottom.png')}  style={myStyles.utfprImage} />
       </View>
       </View>
         </ImageBackground>
--- src/pages/Contato/styles.js
+++ src/pages/Contato/styles.js
@@ -1,137 +1,118 @@
-import { StyleSheet, Dimensions } from 'react-native';
+import { StyleSheet, useWindowDimensions } from 'react-native';
 
-const { width, height } = Dimensions.get('window');
+export const styles = () => {
+  const { width, height } = useWindowDimensions(); // Pegando width e height dinamicamente
 
-export const styles = StyleSheet.create({
-
-  scrollViewContent: {
-    width: width,
-    height: '100%',
-  },
-  backgroundImage: {
-    width: width,
-    height: (width * 1024) / 1440,
-    resizeMode: 'contain',
-  },
-
-  navbar: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    height: width * 0.0729166666666667,
-    backgroundColor: '#195439',
-    position: 'absolute',
-    top: 0,
-    right: 0,
-    width: '100%',
-    zIndex: 2,
-  },
-
-  navbarButton: {
-    fontSize: width * 0.0166666666666667,
-    color: '#FFF',
-    fontWeight: 'bold'
-  },
-
-  navbar2: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-    height: width * 0.06814583333333333333333333333333,
-    backgroundColor: '#166034',
-    position: 'absolute',
-    bottom: 0,
-    right: 0,
-    width: '100%',
-   bottom: 0,
-  },
-
- 
-  araucarias: {
-    width: width * 0.1651041666666667,
-    height: width * 0.146875,
-    marginRight: width * 0.0260416666666667,
-  },
-
-  araucarias2: {
-    zIndex: -2,
-    width: width * 0.1155729166666667,
-    height: width * 0.1028125,
-
-  },
-
-  imageContainer22: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    alignItems: 'center', 
-
-  },
-
-  imageContainer33: {
-    top: width * 0.52,
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    left: width * 0.1041666666666667,
-    marginRight: width * 0.1041666666666667,
-  },
-  imageContainer44: {
-    zIndex:-1,
-    top: width * 0.41,
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    marginRight: width * 0.1380208333333333,
-  },
-
-  utfprImage: {
-    width: 144 * (width * 0.00067708333),
-    height:57 * (width * 0.00067708333),
-    marginLeft: width * 0.0208333333333333,
-  },
-
-  card: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    zIndex: 1,
-    position: 'absolute',
-    justifyContent: 'space-around',
-    top: '44%',
-    left: '30%',
-    width: '40%',
-    height: '28%',
-  },
-  
-  buttonEmail: {
-    width: width * 0.09375,
-    height: width * 0.09375,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  buttonInstagram: {
-    width: width * 0.09375,
-    height: width * 0.09375,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-
-  buttonText: {
-    textAlign: 'center', // Centraliza horizontalmente o texto dentro do botão
-    fontSize: width * 0.021875,
-    color: 'black',
-    bottom: width * -0.0052083333333333, 
-  },
-
-  buttonText2: {
-    textAlign: 'center', // Centraliza horizontalmente o texto dentro do botão
-    fontSize: width * 0.021875,
-    color: 'black',
-    bottom: width * -0.0052083333333333, 
-    left: width * 0.001
-  },
-
-
-  
-});
+  return StyleSheet.create({
+    scrollViewContent: {
+      width: width,
+      height: '100%',
+    },
+    backgroundImage: {
+      width: width,
+      height: (width * 1024) / 1440,
+      resizeMode: 'contain',
+    },
+    navbar: {
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      alignItems: 'center',
+      height: width * 0.0729,
+      backgroundColor: '#195439',
+      position: 'absolute',
+      top: 0,
+      right: 0,
+      width: '100%',
+      zIndex: 2,
+    },
+    navbarButton: {
+      fontSize: width * 0.0167,
+      color: '#FFF',
+      fontWeight: 'bold',
+    },
+    navbar2: {
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      alignItems: 'center',
+      height: width * 0.0681,
+      backgroundColor: '#166034',
+      position: 'absolute',
+      bottom: 0,
+      right: 0,
+      width: '100%',
+    },
+    araucarias: {
+      width: width * 0.1651,
+      height: width * 0.1469,
+      marginRight: width * 0.0260,
+    },
+    araucarias2: {
+      zIndex: -2,
+      width: width * 0.1156,
+      height: width * 0.1028,
+    },
+    imageContainer22: {
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+    },
+    imageContainer33: {
+      top: width * 0.52,
+      flexDirection: 'row',
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+      left: width * 0.1042,
+      marginRight: width * 0.1042,
+    },
+    imageContainer44: {
+      zIndex: -1,
+      top: width * 0.41,
+      flexDirection: 'row',
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+      marginRight: width * 0.1380,
+    },
+    utfprImage: {
+      width: 144 * (width * 0.000677),
+      height: 57 * (width * 0.000677),
+      marginLeft: width * 0.0208,
+    },
+    card: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      zIndex: 1,
+      position: 'absolute',
+      justifyContent: 'space-around',
+      top: '44%',
+      left: '30%',
+      width: '40%',
+      height: '28%',
+    },
+    buttonEmail: {
+      width: width * 0.09375,
+      height: width * 0.09375,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    buttonInstagram: {
+      width: width * 0.09375,
+      height: width * 0.09375,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    buttonText: {
+      textAlign: 'center',
+      fontSize: width * 0.0219,
+      color: 'black',
+      bottom: width * -0.0052,
+    },
+    buttonText2: {
+      textAlign: 'center',
+      fontSize: width * 0.0219,
+      color: 'black',
+      bottom: width * -0.0052,
+      left: width * 0.001,
+    },
+  });
+};
